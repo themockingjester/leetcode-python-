bc8a56a9b338fcedece6b7615b85b335d2ee9b17
Yash.mathur <44976021+themockingjester@users.noreply.github.com>
2021-04-16 09:42:17 +0530

    python3

---
+++ 1305. All Elements in Two Binary Search Trees.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def check(self,root):
+        if root:
+            self.lis.append(root.val)
+            self.check(root.left)
+            self.check(root.right)
+    def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
+        self.lis = []
+        self.check(root1)
+        self.check(root2)
+        return sorted(self.lis)
