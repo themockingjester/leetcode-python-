2a1e16add3a2919c74cacfbe5fea6dd88665563f
Yash.mathur <44976021+themockingjester@users.noreply.github.com>
2021-05-08 05:28:31 +0530

    python3

---
+++ 260. Single Number III.py
@@ -0,0 +1,22 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        lis=[]
+        start=0
+        end=len(nums)-1
+        while start<=end:
+            if start==end:
+                if nums[start] in lis:
+                    lis.remove(nums[start])
+                else:
+                    lis.append(nums[start])
+            if nums[start] in lis:
+                lis.remove(nums[start])
+            else:
+                lis.append(nums[start])
+            if nums[end] in lis:
+                lis.remove(nums[end])
+            else:
+                lis.append(nums[end])
+            start+=1
+            end-=1
+        return lis
