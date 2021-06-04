928a5e3a02319b98b6b57e3ebe5c126b1a896a4a
Yash.mathur <44976021+themockingjester@users.noreply.github.com>
2021-06-04 11:50:34 +0530

    python3

---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+                                
+    def climbStairs(self, n: int) -> int:
+        if n==1:
+            return 1
+        
+        arr = [0 for i in range(n+1)]
+        arr[1]=1
+        arr[2]=2
+        for i in range(3,n+1):
+            arr[i]=arr[i-1]+arr[i-2]
+        return arr[n]
