46a60466d131d280a715d3e6498cf2662b510c56
Yash.mathur <44976021+themockingjester@users.noreply.github.com>
2021-07-10 23:22:06 +0530

    python3

---
+++ 414. Third Maximum Number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums=set(nums)
+        nums =sorted(nums)
+        
+        if len(nums)>=3:
+            
+            return nums[-3]
+        else:
+            return nums[-1]
