6f913c023cd86b75d557cff38be667dfc3342e24
Yash.mathur <44976021+themockingjester@users.noreply.github.com>
2021-07-25 10:17:51 +0530

    Update 322. Coin Change.py

--- 322. Coin Change.py
+++ 322. Coin Change.py
@@ -1,3 +1,4 @@
+
 class Solution:
     def minCoins(self,coins,amount):
         if amount == 0:
@@ -25,6 +26,23 @@ class Solution:
         return out
         
         
+        
+        #                                                               Another approach by me
+        
+    class Solution:
+        def coinChange(self, coins: List[int], amount: int) -> int:
+            dp=[999999999]*(amount+1)
+            dp[0]=0
+            for i in coins:
+                for j in range(1,amount+1):
+                    if j-i<0:
+                        continue
+                    else:
+                        dp[j]=min(dp[j],dp[j-i]+1)
+            if dp[-1]==999999999:
+                return -1
+            else:
+                return dp[-1]
         
         
         
