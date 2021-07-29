3f1f54f6ec2291dc5825183374d0b4565cc310e6
Yash.mathur <44976021+themockingjester@users.noreply.github.com>
2021-07-29 22:04:09 +0530

    Update 300. Longest Increasing Subsequence.py

--- 300. Longest Increasing Subsequence.py
+++ 300. Longest Increasing Subsequence.py
@@ -11,3 +11,21 @@ class Solution:
                     if nums[j]>nums[i]:
                         arr[j]=max(arr[i]+1,arr[j])
         return max(arr)
+       
+#                                                               Another approach by me 
+#                                                       time complexity-O(N^2)            Space Complexity-O(N)
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        val = nums
+        n=len(nums)
+        dp=[1]*(n)
+        for i in range(n):
+
+            for j in range(n):
+                if j<=i:
+                    continue
+                else:
+                    if val[j]>val[i]:
+                        dp[j]=max(dp[j],dp[i]+1)
+
+        return(max(dp))
