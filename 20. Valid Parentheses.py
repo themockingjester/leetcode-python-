9c52e7132efc64c46d3277d0b92fbf4a6699933e
Yash.mathur <44976021+themockingjester@users.noreply.github.com>
2021-08-04 10:55:10 +0530

    added new and best approach

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -21,6 +21,33 @@ class Solution:
         print(must)
         if len(must)==0:
             return True
+      
+                                            # Another Approach (Easy and Faster)
+class Solution:
+    def isValid(self, s: str) -> bool:
+        arr=[]
+        dic={
+            ')':'(',
+            '}':'{',
+            ']':'[',
+        }
+        for i in s:
+            if i==')' and len(arr)!=0 and arr[-1]==dic[')']:
+                arr.pop()
+            elif i=='}' and len(arr)!=0 and arr[-1]==dic['}']:
+                arr.pop()
+            elif i==']' and len(arr)!=0 and arr[-1]==dic[']']:
+                arr.pop()
+            else:
+                arr.append(i)
+        
+        if len(arr)==0:
+            return True
+        else:
+            return False
+            
+            
+                
             
             
                 
