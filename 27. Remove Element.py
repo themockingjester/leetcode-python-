6acea8003ed4c33558d20a651310db3f8fd96246
Yash Mathur <44976021+themockingjester@users.noreply.github.com>
2024-01-30 16:37:37 +0530

    Update 27. Remove Element.py

--- 27. Remove Element.py
+++ 27. Remove Element.py
@@ -10,4 +10,19 @@ class Solution:
         for i in lis:
             del nums[i-ctr]
             ctr+=1
+
+# Another
+# Space- O(N)
+# Time - O(N)
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i=0
+        different=0
+        while i<len(nums):
+            if(nums[i]!=val):
+                different+=1
+                i+=1
+            else:
+                nums
+        return different
             
