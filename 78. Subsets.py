24de9d0edc0653e2f0cf7f82ac5567fa8ca6ab72
Yash Mathur <44976021+themockingjester@users.noreply.github.com>
2024-01-31 22:30:59 +0530

    Update 78. Subsets.py

--- 78. Subsets.py
+++ 78. Subsets.py
@@ -14,3 +14,27 @@ class Solution:
         self.res=[]
         self.solve(len(nums),[],0,nums)
         return self.res
+
+# With pure recursion
+class Solution:
+    def __init__(self):
+        self.result=[]
+        self.found={}
+    def subsetsSol(self,index,currArr,nums):
+
+        copiedArr = currArr.copy()
+        if index>= len(nums):
+            self.result.append(copiedArr)
+            return
+        # pick
+        copiedArr.append(nums[index])
+        self.subsetsSol(index+1,copiedArr,nums)
+        copiedArr.pop()
+
+        # not pick
+        self.subsetsSol(index+1,copiedArr,nums)
+
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        self.subsetsSol(0,[],nums)
+        return self.result
