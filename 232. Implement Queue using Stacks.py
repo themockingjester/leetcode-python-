e645178975a3cd08b757d0810ab8bf0fd4265199
Yash Mathur <44976021+themockingjester@users.noreply.github.com>
2024-02-23 22:15:47 +0530

    Update 232. Implement Queue using Stacks.py

--- 232. Implement Queue using Stacks.py
+++ 232. Implement Queue using Stacks.py
@@ -1,3 +1,43 @@
+# First solution
+
+class MyQueue:
+
+    def __init__(self):
+        self.inputQueue = []
+        self.outputQueue=[]
+
+    def push(self, x: int) -> None:
+        self.inputQueue.append(x)
+
+    def pop(self) -> int:
+        if len(self.outputQueue)>0:
+            return self.outputQueue.pop()
+        else:
+            while len(self.inputQueue)>0:
+                self.outputQueue.append(self.inputQueue.pop())
+            return self.outputQueue.pop()
+
+    def peek(self) -> int:
+        if len(self.outputQueue)>0:
+            return self.outputQueue[-1]
+        else:
+            while len(self.inputQueue)>0:
+                self.outputQueue.append(self.inputQueue.pop())
+            return self.outputQueue[-1]
+
+    def empty(self) -> bool:
+        return len(self.inputQueue)==0 and len(self.outputQueue)==0
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
+
+
+# Another solution
 class MyQueue:
     class Node():
         def __init__(self):
