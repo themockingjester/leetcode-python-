9e8e17f04f15b6018d1d3b1b7bf226e59afe54ce
Yash Mathur <44976021+themockingjester@users.noreply.github.com>
2024-02-24 08:55:45 +0530

    Update 496. Next Greater Element I.py

--- 496. Next Greater Element I.py
+++ 496. Next Greater Element I.py
@@ -1,3 +1,32 @@
+# with stack
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        myStack = []
+        ngeArr=[]
+        for i in range(len(nums2)-1,-1,-1):
+            curr = nums2[i]
+            currAns = -1
+            while len(myStack)>0:
+                if myStack[-1]<=curr:
+                    myStack.pop()
+                else:
+                    currAns = myStack[-1]
+                    myStack.append(curr)
+                    break
+            else:
+                myStack.append(curr)
+            ngeArr.append(currAns)
+        ngeArr=ngeArr[::-1]
+        result=[]
+        for i in nums1:
+            result.append(ngeArr[nums2.index(i)])
+        return result
+            
+
+
+        
+
+# Another
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         lis = []
