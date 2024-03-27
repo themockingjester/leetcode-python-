903c063638293cd07faba1bc4fb447d76045a4fa
Yash Mathur <44976021+themockingjester@users.noreply.github.com>
2024-03-28 01:15:05 +0530

    Update 142. Linked List Cycle II.py

--- 142. Linked List Cycle II.py
+++ 142. Linked List Cycle II.py
@@ -4,13 +4,37 @@
 #         self.val = x
 #         self.next = None
 
+
+############################      With Two pointer
 class Solution:
-    def detectCycle(self, head: ListNode) -> ListNode:
-        dic = {}
-        while head:
-            if head not in dic:
-                dic[head]=1
+    def detectLoop(self,head):
+        self.slow=head
+        self.fast=head
+        while True:
+            if self.slow==None:
+                return False
+            elif self.slow.next==None:
+                return False
             else:
-                return head
-            head = head.next
-            
+                self.slow = self.slow.next
+            if self.fast==None:
+                return False
+            elif self.fast.next!=None and self.fast.next.next!=None:
+                self.fast=self.fast.next.next
+            else:
+                return False
+            if self.fast==self.slow:
+                return True
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        hasLoop = self.detectLoop(head)
+        if(hasLoop):
+            ind=0
+            start=head
+            slow2=self.slow
+            while start!=slow2:
+                
+                start=start.next
+                slow2=slow2.next
+                ind+=1
+            return start
+        return None
